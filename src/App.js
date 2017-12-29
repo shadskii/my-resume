4aa02d52178f2cfcd589d99d70f66d7d283d0320
shadskii <jake.k.hassel@gmail.com>
2017-12-28 20:00:50 -0500

    Implementing pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { SectionsContainer, Section, Header, Footer } from 'react-fullpage';
+import jake from './jake.jpg'
+
 
 class App extends Component {
   render() {
+    let options = {
+      sectionClassName: 'section',
+      anchors: ['sectionOne', 'sectionTwo', 'sectionThree'],
+      delay: 500,
+      scrollBar: false,
+      navigation: true,
+      verticalAlign: false,
+      sectionPaddingTop: '50px',
+      sectionPaddingBottom: '50px',
+      arrowNavigation: true
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='container-fluid'>
+        <Header>
+          <a href="#sectionOne">Section One</a>
+          <a href="#sectionTwo">Section Two</a>
+          <a href="#sectionThree">Section Three</a>
+        </Header>
+        <SectionsContainer  {...options}>
+          <Section verticalAlign="true" >
+            <div>
+              <div className='row col'>
+                <img className='img-circle ' src={jake} />
+              </div>
+
+              <div className='row col'>
+                <h1 ><span className='label label-primary'>Developer</span></h1>
+              </div>
+            </div>
+          </Section>
+          <Section color="#A7DBD8">Page 2</Section>
+          <Section color="#E0E4CC">Page 3</Section>
+        </SectionsContainer>
       </div>
     );
   }
