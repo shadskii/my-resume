9b83cac407a191f2ca1b860ea045041990d9c3d2
shadskii <jake.k.hassel@gmail.com>
2018-01-01 18:57:57 -0500

    Centered

--- src/components/SkillsPage.jsx
+++ src/components/SkillsPage.jsx
@@ -30,7 +30,10 @@ class SkillsPage extends Component {
                 <h1 className='row col'>Skills</h1>
                 <div className='row'>
                     {skills.map(skill =>
-                        <Card className='col-lg-3' style={style}>
+                        <Card
+                            key={skill}
+                            className='col-lg-3 col-sm-10'
+                            style={style}>
                             <CardHeader
                                 title={skill}
                                 avatar={<i class="fa fa-check-square-o fa-2x" aria-hidden="true"></i>}
