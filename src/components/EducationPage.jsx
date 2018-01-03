9366a78462c09ff7c67f964253a95c7dc9a97166
shadskii <jake.k.hassel@gmail.com>
2018-01-03 01:52:18 -0500

    Simple education page

---
+++ src/components/EducationPage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import lehigh from '../LU.png';
+
+class EducationPage extends Component {
+
+    render() {
+        return (
+            <div><img src={lehigh} /></div>
+        );
+    }
+}
+export default EducationPage;
