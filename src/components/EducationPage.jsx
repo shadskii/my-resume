14a23b95de07b92a46e25c78662c82b6409ba107
shadskii <jake.k.hassel@gmail.com>
2018-01-04 20:03:41 -0500

    Added lehigh

--- src/components/EducationPage.jsx
+++ src/components/EducationPage.jsx
@@ -12,6 +12,7 @@ class EducationPage extends Component {
                 <div className='row'>
                     <ul className='col-lg-6 list-info'>
                         <h1 className='page-title'>Education</h1>
+                        <li className='list-item'> Lehigh University</li>
                         <li className='list-item'>Computer Science and Engineering</li>
                         <li className='list-item' >P.C. Rosin College of Engineering</li>
                         <li className='list-item'>Class of 2016</li>
